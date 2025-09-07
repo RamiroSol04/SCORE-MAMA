0b483f290c3c71e54d5e718390c58e39325da700
RamiroSol04 <ramiro1981sol@gmail.com>
2025-09-06 20:36:33 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+{
+"name": "Calculadora Score MAMÁ",
+"short_name": "Score MAMÁ",
+"description": "Una herramienta para calcular el Score MAMÁ y obtener indicaciones rápidas.",
+"start_url": "/index.html",
+"display": "standalone",
+"background_color": "#f3f4f6",
+"theme_color": "#60a5fa",
+"icons": [
+{
+"src": "https://www.google.com/search?q=https://placehold.co/192x192/4b5563/ffffff%3Ftext%3DIcono",
+"sizes": "192x192",
+"type": "image/png"
+},
+{
+"src": "https://www.google.com/search?q=https://placehold.co/512x512/4b5563/ffffff%3Ftext%3DIcono",
+"sizes": "512x512",
+"type": "image/png"
+}
+]
+}
